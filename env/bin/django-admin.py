df11efc1605811ad633180973527ca03b038f41c
nova-sangeeth <novasangeeth@outlook.com>
2020-06-19 16:32:06 +0530

    second commit

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/mnt/e/Developement/bank_application/env/bin/python3.7
+#!/mnt/e/Developement/bank_application/env/bin/python
 from django.core import management
 
 if __name__ == "__main__":
--- env/lib/python3.7/random.py
+++
@@ -1,1 +0,0 @@
-/home/nova_sangeeth/anaconda3/lib/python3.7/random.py
