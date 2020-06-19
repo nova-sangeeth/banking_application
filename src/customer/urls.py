98cd26a5017c0132fdf0dff56e4488087a0e90c6
nova-sangeeth <novasangeeth@outlook.com>
2020-06-19 17:51:22 +0530

    fixed issue with url's not reversing

--- src/customer/urls.py
+++ src/customer/urls.py
@@ -4,10 +4,10 @@ from .views import index, register, profile, edit, withdraw, deposit
 
 
 urlpatterns = [
-    url(r'^$', index, name="index"),
-    url(r'^register$', register, name="register"),
-    url(r'^profile$', profile, name="profile"),
-    url(r'^edit$', edit, name="edit"),
-    url(r'^withdraw$', withdraw, name="withdraw"),
-    url(r'^deposit$', deposit, name="deposit"),
+    url(r"^$", index, name="index"),
+    url(r"^register/$", register, name="register"),
+    url(r"^profile/$", profile, name="profile"),
+    url(r"^edit/$", edit, name="edit"),
+    url(r"^withdraw/$", withdraw, name="withdraw"),
+    url(r"^deposit/$", deposit, name="deposit"),
 ]
