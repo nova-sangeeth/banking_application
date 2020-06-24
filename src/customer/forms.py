7da9f509830b174c7214acaf87627546ea5b8b06
nova-sangeeth <novasangeeth@outlook.com>
2020-06-24 21:55:44 +0530

    Made changes to the forms and removed all the specific widgets to the form

--- src/customer/forms.py
+++ src/customer/forms.py
@@ -6,16 +6,17 @@ from .models import customer
 class customer_details_form(forms.ModelForm):
     class Meta:
         model = customer
-        widgets = {
-            "name": forms.TextInput(attrs={'required': "required"}),
-            "age": forms.NumberInput(attrs={'required': "required"}),
-            "street": forms.TextInput(attrs={'required': "required"}),
-            "city": forms.TextInput(attrs={'required': "required"}),
-            "phone": forms.TextInput(attrs={'required': "required"}),
-            "state": forms.TextInput(attrs={'required': "required"}),
-            "country": forms.TextInput(attrs={'required': "required"}),
-            "pincode": forms.NumberInput(attrs={'required': "required"}),
-            "balance": forms.NumberInput(attrs={'required': "required"}),
-        }
-        fields = ['name', 'age', 'phone', 'street', 'city',
-                  'state', 'country', 'pincode', 'balance']
+
+        fields = [
+            "name",
+            "age",
+            "phone",
+            "street",
+            "city",
+            "state",
+            "country",
+            "pincode",
+            "balance",
+        ]
+        exclude = ("user",)
+
